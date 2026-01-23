429951ac53f344235d42af3ce7697eafed1e5ceb
CaioFelps007 <125682381+CaioFelps007@users.noreply.github.com>
2026-01-23 20:23:35 -0300

    commit2

--- menteliphones/src/Pages/Home/home.jsx
+++ menteliphones/src/Pages/Home/home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import NavHeader from "../../components/header/header";
-import logomentel from "../../images/logopimenta.jpeg";
 import "./home.css";
+
+//images
+import logomentel from "../../images/logopimenta.jpeg";
+import iphone1 from "../../images/iphone1.jpg";
+import iphone2 from "../../images/iphone2.jpg";
+import iphone3 from "../../images/iphone3.jpg";
 
 function Home() {
   return (
@@ -17,7 +22,32 @@ function Home() {
             />
           </div>
         </section>
-        <section>Continua</section>
+        <section className="hero-imperdiveis">
+          <div className="promotion">
+            <h2>IMPERDIVEIS DO MOMENTO</h2>
+            <div className="img-promotion">
+              <img src={iphone1} className="iphonesdestack" />
+              <img src={iphone2} className="iphonesdestack" />
+              <img src={iphone3} className="iphonesdestack" />
+            </div>
+          </div>
+        </section>
+        <section className="hero-textone">
+          <div className="explication">
+            <h2>COMPRE CONOSCO</h2>
+            <p>
+              Melhores precos da regiao, fazendo por amor em ver o sorriso no
+              rosto de outras pessoas ao realizar um sonho que a maioria hoje
+              tem, compre seu iphone!
+            </p>
+          </div>
+        </section>
+        <section className="hero-avalia">
+          <div className="avaliacoes-stars">
+            <h2>VEJA ALGUNS DOS CLIENTES SATISFEITOS</h2>
+          </div>
+          <div className="comentarios-avaliacoes">NOTA 10 ATENDIMENTO</div>
+        </section>
       </main>
     </>
   );
